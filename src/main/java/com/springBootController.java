45d79d241410708690846687dd6780c08db86ed2
15874271547 <765954133@qq.com>
2017-08-02 20:03:29 +0800

    跳转JSP页面 XML文件配置 依赖文件配置

--- src/main/java/com/springBootController.java
+++ src/main/java/com/springBootController.java
@@ -1,28 +1,29 @@
+package com;
 
 import org.springframework.boot.*;
-		import org.springframework.boot.autoconfigure.*;
-		import org.springframework.stereotype.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.*;
 		import org.springframework.web.bind.annotation.*;
 
 //表明这是一个 Controller
-@Controller
 
 //RestController是一种Rest风格的Controller，可以直接返回对象而不返回视图，返回的对象可以使JSON，XML等
 //@RestController
-
+@SpringBootApplication
+@Controller
 //使用自动配置，主动添加并解析bean，配置文件等信息
-@EnableAutoConfiguration
-
 public class springBootController {
 
 	//设置访问的url
-	@RequestMapping("/")
-	//表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
-	@ResponseBody
-	String home() {
-		return "Hello Worldss!";//返回结果为字符串
-	}
+	@Controller
+	public class HelloController {
+		@RequestMapping("/index")
+		//表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
+		public String index() {
 
+			return "index";//返回结果为字符串
+		}
+	}
 	public static void main(String[] args) throws Exception {
 		//通过SpringApplication的run()方法启动应用，无需额外的配置其他的文件
 		SpringApplication.run(springBootController.class, args);
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <title>title</title>
+
+
+</head>
+
+<body>
+
+
+asdasdasdasd
+
+</body>
+</html>
