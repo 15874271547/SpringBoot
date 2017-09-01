f6198af9c911d62d2809bb16f26482a8ea42ab82
15874271547 <765954133@qq.com>
2017-09-01 10:12:21 +0800

    跳转JSP页面 XML文件配置 依赖文件配置

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by admin on 2017/9/1.
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
